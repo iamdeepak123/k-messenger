a29e526a0dcc8918e0c9df3650bca7cceee8af23
Deepak Karki <deepakkarki.work@gmail.com>
2024-08-12 15:55:29 +0530

    Updated ui

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -5,10 +5,19 @@ import ChatSection from "../Components/ChatSection"
 
 const Homepage = () => {
   return (
-    <div className='Home_container'>
-      <SideBar />
-      <ChatSection />
-    </div>
+    <>
+      <div className='Home_Messgaesbox'>
+        <h3>This App is not available for Mobile Version.</h3>
+        <h3>We are currently working on it.</h3>
+        <h3> Please Switch to Desktop View till then</h3>
+      </div>
+      <div className='Home_container'>
+        <SideBar />
+        <ChatSection />
+      </div>
+
+    </>
+
   )
 }
 
